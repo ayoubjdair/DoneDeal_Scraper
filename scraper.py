9816c2734ea802b3a90f97ca7d9e84c93c66ae2d
Ayoub <ayoub324@icloud.com>
2022-05-19 03:15:14 +0100

    GUI implemented - links functionality and auto next page

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+from ctypes import resize
+import webbrowser
 import requests
 import PySimpleGUI as sg
 from bs4 import BeautifulSoup
@@ -12,173 +14,102 @@ locations = []
 prices = []
 links = []
 
-result = requests.get(
-    "https://www.donedeal.ie/cars/Volkswagen/Scirocco?price_to=15000&sort=publishdate%20desc"
-)
-
-result2 = requests.get(
-    "https://www.donedeal.ie/cars/Volkswagen/Scirocco?price_to=15000&sort=publishdate%20desc&start=28"
-)
-
-
-result3 = requests.get(
-    "https://www.donedeal.ie/cars/Volkswagen/Scirocco?price_to=15000&sort=publishdate%20desc&start=60"
-)
-
-src = result.content
-src2 = result2.content
-src3 = result3.content
-
-soup = BeautifulSoup(src, 'html.parser')
-soup2 = BeautifulSoup(src2, 'html.parser')
-soup3 = BeautifulSoup(src3, 'html.parser')
-
-listing_titles = soup.find_all('p', attrs={'class' : 'card__body-title'})
-listing_titles2 = soup2.find_all('p', attrs={'class' : 'card__body-title'})
-listing_titles3 = soup3.find_all('p', attrs={'class' : 'card__body-title'})
-
-listing_details = soup.find_all('ul', attrs={'class' : 'card__body-keyinfo'})
-listing_details2 = soup2.find_all('ul', attrs={'class' : 'card__body-keyinfo'})
-listing_details3 = soup3.find_all('ul', attrs={'class' : 'card__body-keyinfo'})
-
-listing_prices = soup.find_all('div', attrs={'class' : 'card__price--left-options'})
-listing_prices2 = soup2.find_all('div', attrs={'class' : 'card__price--left-options'})
-listing_prices3 = soup3.find_all('div', attrs={'class' : 'card__price--left-options'})
-
-listing_links = soup.find_all('a', attrs={'class' : 'card__link'})
-listing_links2 = soup2.find_all('a', attrs={'class' : 'card__link'})
-listing_links3 = soup3.find_all('a', attrs={'class' : 'card__link'})
-
-for listing in listing_titles:
-    try:
-        title = listing.text
-        titles.append(title)
-    except:
-        print("Oops! no Title Found...")
-        continue
-
-for listing in listing_details:
-    try:
-        years.append(listing.text[:4])
-        engines.append(listing.text[4:14])
-        milage.append(listing.text[14:23])
-        locations.append(listing.text[23:])
-    except:
-        print("Error fetching details...")
-        continue
+def DrawGUI():
     
-for listing in listing_prices:
-    try:
-        price = listing.find('p', attrs={'class' : 'card__price'}).text
-        prices.append(price)
-    except:
-        print("Error fetching price...")
-        continue
+    sg.theme('Dark')
     
-for listing in listing_links:
-    try:
-        links.append(listing.attrs['href'])
-    except:
-        print("Error fetching link...")
-        continue
-    
-# Page 2
-for listing in listing_titles2:
-    try:
-        title = listing.text
-        titles.append(title)
-    except:
-        print("Oops! no Title Found...")
-        continue
+    layout = [
+        [sg.Text('Enter DoneDeal Link '), sg.Input(default_text='Example: https://www.donedeal.ie/cars/Make/Model', size=(88, 1), key='link')],
+        [sg.Output(size=(120, 20), font='Courier 12')],
+        [sg.Button('Scrape')],
+        [sg.Input(default_text='Listing #', size=(8, 1), key='listing'), sg.Button('View Online')],
+        [sg.Button('Exit', button_color=('white', 'firebrick3'))]
+    ]
 
-for listing in listing_details2:
-    try:
-        years.append(listing.text[:4])
-        engines.append(listing.text[4:14])
-        milage.append(listing.text[14:23])
-        locations.append(listing.text[23:])
-    except:
-        print("Error fetching details...")
-        continue
-    
-for listing in listing_prices2:
-    try:
-        price = listing.find('p', attrs={'class' : 'card__price'}).text
-        prices.append(price)
-    except:
-        print("Error fetching price...")
-        continue
-    
-for listing in listing_links2:
-    try:
-        links.append(listing.attrs['href'])
-    except:
-        print("Error fetching link...")
-        continue
-    
-    
-# Page 3
-for listing in listing_titles3:
-    try:
-        title = listing.text
-        titles.append(title)
-    except:
-        print("Oops! no Title Found...")
-        continue
+    window = sg.Window("DoneDeal Scraper", layout)
 
-for listing in listing_details3:
-    try:
-        years.append(listing.text[:4])
-        engines.append(listing.text[4:14])
-        milage.append(listing.text[14:23])
-        locations.append(listing.text[23:])
-    except:
-        print("Error fetching details...")
-        continue
-    
-for listing in listing_prices3:
-    try:
-        price = listing.find('p', attrs={'class' : 'card__price'}).text
-        prices.append(price)
-    except:
-        print("Error fetching price...")
-        continue
-    
-for listing in listing_links3:
-    try:
-        links.append(listing.attrs['href'])
-    except:
-        print("Error fetching link...")
-        continue
-    
-for x in range(len(titles)):
-    cars.append([titles[x], years[x], engines[x], milage[x], locations[x], prices[x], links[x]])
-    # print(titles[x])
-    # print(years[x])
-    # print(engines[x])
-    # print(milage[x])
-    # print(locations[x])
+    while True:
+        event, values = window.read()
+        if event == 'Exit' or event == sg.WIN_CLOSED:
+            break
+        if event == 'Scrape':
+            if values['link'] == 'Example: https://www.donedeal.ie/cars/Make/Model' or values['link'] == '':
+                print("Please Enter a valid link (Like the example above)")
+                window.refresh()
+            else:
+                link = values['link']
+                output = runScraper(link)
+                window.refresh()
+        if event == 'View Online':
+            if values['listing'] == 'Listing #' or values['listing'] == '':
+                print("Please Enter a valid listing number")
+                window.refresh()
+            else:
+                index = int(values['listing'])
+                webbrowser.open(links[index])
+            
+    window.close()
 
+def runScraper(link):
+    root = link
+    pageStart = "&start="
 
-print('                    TITLE                           YEAR    ENGINE     MILAGE         LOCATION        PRICE    LINKS')
-print(tabulate(cars))
-
-layout = [
-    [sg.Text("DoneDeal Scraper GUI")],
-    [sg.Output(size=(90, 20), font='Courier 12')],
-    [sg.Button('Scrape')],
-    [sg.Button('Exit')]
-]
-
-window = sg.Window("DoneDeal Scraper", layout, margins=(200,100)).read()
-
-while True:
-    event, values = window.read()
-    if event == 'Exit' or event == sg.WIN_CLOSED:
-        break
-    if event == 'Scrape':
-        # output = print(tabulate(cars))
-        print("hello world")
-        window.refresh()
+    for x in range(1, 115, 23):
+        src = setResult(x, root, pageStart).content
         
-window.close()
+        soup = BeautifulSoup(src, 'html.parser')
+
+        listing_titles = soup.find_all('p', attrs={'class' : 'card__body-title'})
+        listing_details = soup.find_all('ul', attrs={'class' : 'card__body-keyinfo'})
+        listing_prices = soup.find_all('div', attrs={'class' : 'card__price--left-options'})
+        listing_links = soup.find_all('a', attrs={'class' : 'card__link'})
+
+
+        for listing in listing_titles:
+            try:
+                title = listing.text
+                titles.append(title)
+            except:
+                print("Oops! no Title Found...")
+                continue
+
+        for listing in listing_details:
+            try:
+                years.append(listing.text[:4])
+                engines.append(listing.text[4:14])
+                milage.append(listing.text[14:23])
+                locations.append(listing.text[23:])
+            except:
+                print("Error fetching details...")
+                continue
+            
+        for listing in listing_prices:
+            try:
+                price = listing.find('p', attrs={'class' : 'card__price'}).text
+                prices.append(price)
+            except:
+                print("Error fetching price...")
+                continue
+            
+        for listing in listing_links:
+            try:
+                links.append(listing.attrs['href'])
+            except:
+                print("Error fetching link...")
+                continue
+
+
+    for x in range(len(titles)):
+        cars.append([x, titles[x], years[x], engines[x], milage[x], locations[x], prices[x]])
+
+    print(' #                       TITLE                          YEAR    ENGINE      MILAGE        LOCATION       PRICE    ')
+    print(tabulate(cars))
+
+def setResult(start, root, pageStart):
+    result = requests.get(
+        root + pageStart + str(start)
+    )
+    return result
+
+if __name__ == '__main__':
+    DrawGUI()
